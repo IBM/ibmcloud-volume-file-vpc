c747e32bcee35a9b55e8ed92a659ba4ef3d202e6
Sameer Shaikh <sameer.shaikh@ibm.com>
2023-10-05 15:48:17 +0530

    Adding support for GetSecurityGroupByName
    Signed-off-by: Sameer Shaikh <sameer.shaikh@ibm.com>

--- file/provider/get_subnet_test.go
+++ file/provider/get_subnet_test.go
@@ -1,9 +1,13 @@
 /**
 <<<<<<< HEAD
+<<<<<<< HEAD
  * Copyright 2023 IBM Corp.
 =======
  * Copyright 2021 IBM Corp.
 >>>>>>> adfefb0 (ENI support)
+=======
+ * Copyright 2023 IBM Corp.
+>>>>>>> 2dc2601 (Adding support for GetSecurityGroupByName)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
