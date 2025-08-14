ad554b9ea96f89f1deabe07a5b5ec98a3facc388
Sameer Shaikh <sameer.shaikh@ibm.com>
2025-08-14 21:00:04 +0530

    Support for configurable beta
    Signed-off-by: Sameer Shaikh <sameer.shaikh@ibm.com>

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -120,7 +120,7 @@ function check_trusted_profile_status {
         profileID=$(echo $decoded | grep IBMCLOUD_PROFILEID | cut -d'=' -f3-)
         echo "parsed $profileID"
 		echo "expected $expected_profile_id"
-        if [[ "$profileID" == "$EXPECTED_PROFILE_ID" ]]; then
+        if [[ "$profileID" == "$expected_profile_id" ]]; then
             echo -e "VPC-FILE-CSI-TEST: VERIFYING TRUSTED_PROFILE: PASS" >> $E2E_TEST_SETUP
 			echo "***************************************************" >> $E2E_TEST_SETUP
         else
