ca47a1419b3ba0015276238930366714cbe4854c
Prankul <84079249+prankulmahajan@users.noreply.github.com>
2026-01-21 18:40:36 +0530

    Update create_volume.go
    Signed-off-by: Sameer Shaikh <sameer.shaikh@ibm.com>

--- file/provider/create_volume.go
+++ file/provider/create_volume.go
@@ -91,19 +91,20 @@ func (vpcs *VPCSession) CreateVolume(volumeRequest provider.Volume) (volumeRespo
 			}
 		}
 
-		// if EIT enabled
-		if volumeRequest.TransitEncryption == EncryptionTrasitMode && volumeRequest.VPCVolume.Profile != nil && volumeRequest.VPCVolume.Profile.Name == "dp2" {
-			shareTargetTemplate.TransitEncryption = volumeRequest.TransitEncryption
-		} else {
-			shareTargetTemplate.TransitEncryption = "stunnel"
-		}
-
 		// Set access_protocol and transit_encryption ONLY for 'rfs' profile
 		// Note: These are mandatory parameters for rfs profile
 		if volumeRequest.VPCVolume.Profile != nil && volumeRequest.VPCVolume.Profile.Name == vpcfile.RFSProfile {
 			shareTargetTemplate.AccessProtocol = "nfs4"
 			shareTargetTemplate.TransitEncryption = "none"
 		}
+
+		// if EIT enabled
+		if volumeRequest.TransitEncryption == EncryptionTrasitMode && volumeRequest.VPCVolume.Profile != nil && volumeRequest.VPCVolume.Profile.Name == "dp2" {
+			shareTargetTemplate.TransitEncryption = volumeRequest.TransitEncryption
+		} else if volumeRequest.TransitEncryption == EncryptionTrasitMode && volumeRequest.VPCVolume.Profile.Name == "rfs" {
+			shareTargetTemplate.TransitEncryption = "stunnel"
+		}
+
 
 		volumeAccessPointList := make([]models.ShareTarget, 1)
 		volumeAccessPointList[0] = shareTargetTemplate
