ff85341e0d50004d7ed067782bac176a8c974778
Prankul Mahajan <prankul.mahajan@ibm.com>
2026-07-14 17:49:50 +0530

    revert to not using securityContext in rfs

--- e2e/ginkgo_tests/rfs_test.go
+++ e2e/ginkgo_tests/rfs_test.go
@@ -70,11 +70,9 @@ var _ = Describe("[ics-e2e] [sc_rfs] Dynamic Provisioning for RFS SC with Deploy
 		reclaimPolicy := v1.PersistentVolumeReclaimDelete
 
 		var replicaCount = int32(1)
-		fsGroup := int64(0)
 		pod := testsuites.PodDetails{
-			Cmd:             "echo 'hello world' >> /mnt/test-1/data && while true; do sleep 2; done",
-			CmdExits:        false,
-			SecurityContext: &v1.PodSecurityContext{FSGroup: &fsGroup},
+			Cmd:      "echo 'hello world' >> /mnt/test-1/data && while true; do sleep 2; done",
+			CmdExits: false,
 			Volumes: []testsuites.VolumeDetails{
 				{
 					PVCName:       "ics-vol-rfs-",
@@ -129,11 +127,9 @@ var _ = Describe("[ics-e2e] [sc_rfs] Dynamic Provisioning for RFS SC with Deploy
 		reclaimPolicy := v1.PersistentVolumeReclaimDelete
 
 		var replicaCount = int32(1)
-		fsGroup := int64(0)
 		pod := testsuites.PodDetails{
-			Cmd:             "echo 'hello world' >> /mnt/test-1/data && while true; do sleep 2; done",
-			CmdExits:        false,
-			SecurityContext: &v1.PodSecurityContext{FSGroup: &fsGroup},
+			Cmd:      "echo 'hello world' >> /mnt/test-1/data && while true; do sleep 2; done",
+			CmdExits: false,
 			Volumes: []testsuites.VolumeDetails{
 				{
 					PVCName:       "ics-vol-rfs-",
@@ -194,11 +190,9 @@ var _ = Describe("[ics-e2e] [sc_rfs] Dynamic Provisioning for RFS SC with Deploy
 		reclaimPolicy := v1.PersistentVolumeReclaimDelete
 
 		var replicaCount = int32(1)
-		fsGroup := int64(0)
 		pod := testsuites.PodDetails{
-			Cmd:             "echo 'hello world' >> /mnt/test-1/data && while true; do sleep 2; done",
-			CmdExits:        false,
-			SecurityContext: &v1.PodSecurityContext{FSGroup: &fsGroup},
+			Cmd:      "echo 'hello world' >> /mnt/test-1/data && while true; do sleep 2; done",
+			CmdExits: false,
 			Volumes: []testsuites.VolumeDetails{
 				{
 					PVCName:       "ics-vol-rfs-",
